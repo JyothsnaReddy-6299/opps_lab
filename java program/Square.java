3c998b1ea1220ddcb7ec98416f7086254ff9064b
JyothsnaReddy-6299 <jyothsna.reddy1503@gmail.com>
2025-12-23 18:47:02 +0530

    Add files via upload

---
+++ java program/Square.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Square {
+    public static void main(String args[]){
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int square = n*n;
+    System.out.println("The square of the number is " +square );
+    }
+    
+}
