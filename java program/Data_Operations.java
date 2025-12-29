6a418362485577805bcb53556d2f6892a9607478
JyothsnaReddy-6299 <jyothsna.reddy1503@gmail.com>
2025-12-29 18:04:17 +0530

    Add files via upload

---
+++ java program/Data_Operations.java
@@ -0,0 +1,23 @@
+public class Data_Operations {
+        public static void main(String[] args) {
+
+        // Predefined numbers
+        double num1 = 20;
+        double num2 = 5;
+
+        // Performing operations
+        double add = num1 + num2;
+        double sub = num1 - num2;
+        double mul = num1 * num2;
+        double div = num1 / num2;
+
+        System.out.println("First Number: " + num1);
+        System.out.println("Second Number: " + num2);
+        
+        System.out.println("Addition: " + add);
+        System.out.println("Subtraction: " + sub);
+        System.out.println("Multiplication: " + mul);
+        System.out.println("division: " + div);
+    
+}
+}
